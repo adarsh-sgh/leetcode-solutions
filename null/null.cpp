4c69ff27428f18483cb265b0c2e155a871848d44
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2023-04-20 12:13:47 +0530

    Time: 4 ms (42.94%), Space: 5.8 MB (94.89%) - LeetHub

--- null/null.cpp
+++ null/null.cpp
@@ -1,48 +1,13 @@
-class Trie {
+class Solution {
 public:
-    struct TrieNode{
-        bool eof = false;
-        TrieNode* childs[26] ;
-    };
-    TrieNode *root= new TrieNode();
-    Trie() {
-        
-    }
-    
-    void insert(string word) {
-        TrieNode *curr = root;
-        for(auto &&c:word){
-            if(!curr->childs[c-'a']){
-                curr->childs[c-'a'] = new TrieNode();
-            }
-            curr = curr->childs[c-'a'];
+    double myPow(double x, long long n) {
+        if(n<0){
+            x = 1/x;
+            n = -n;
         }
-        curr->eof = true;
-    }
-    
-    bool search(string word) {
-        TrieNode *curr = root;
-        for(auto &&c:word ){
-            if(!curr->childs[c-'a']) return false;
-            curr = curr->childs[c-'a'];
-        }
-        return curr->eof;
-    }
-    
-    bool startsWith(string prefix) {
-TrieNode *curr = root;
-        for(auto &&c:prefix ){
-            if(!curr->childs[c-'a']) return false;
-            curr = curr->childs[c-'a'];
-        }
-        return true;
+        if(n==0) return 1;
+        double p = myPow(x,n/2);
+        if(n&1) return p*p*x;
+        return p*p;
     }
 };
-
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie* obj = new Trie();
- * obj->insert(word);
- * bool param_2 = obj->search(word);
- * bool param_3 = obj->startsWith(prefix);
- */
