107166bf185c1dc3c711b0bc37360c5858693f2d
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2023-04-29 01:56:00 +0530

    Time: 61 ms (37.93%), Space: 15.6 MB (46.45%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+#define dbg(...) 42;
+    int maximalRectangle(vector<vector<char>>& matrix) {
+  int m = matrix.size();
+  int n = matrix[0].size();
+  vector<vector<int>> h(m, vector<int>(n));
+  for (int i = 0;i < n;i++) {
+    h[0][i] = matrix[0][i] - '0';
+  }
+  for (int i = 1;i < m;i++) {
+    for (int j = 0;j < n;j++) {
+      if (matrix[i][j] == '0') h[i][j] = 0;
+      else h[i][j] = h[i - 1][j] + 1;
+    }
+  }
+  dbg(h);
+  int maxArea = 0;
+  for (int i = 0;i < m;i++) {
+    stack<int>mi;
+    vector<int>pse(n, -1);
+    vector<int>nse(n, n);
+    for (int j = 0;j < n;j++) {
+      while (!mi.empty() && h[i][mi.top()] >= h[i][j]) {
+        auto t = mi.top();
+        nse[t] = j;
+        mi.pop();
+      }
+      if (!mi.empty()) {
+        pse[j] = mi.top();
+      }
+
+      mi.push(j);
+    }
+    dbg(nse, pse);
+    for (int j = 0;j < n;j++) {
+      maxArea = max(maxArea, (nse[j] - pse[j] - 1) * h[i][j]);
+    }
+  }
+  return maxArea;
+}
+};
