c9a17e17a89d5d98edb5a4f9f67df938d6516310
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2023-06-16 14:33:09 +0530

    Time: 89 ms (83.42%) | Memory: 22.9 MB (62.87%) - LeetSync

--- minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     int minimumDeletions(string s) {
   int n = s.size();
-  vector<vector<int>>dp(n + 1, vector<int>(2));
+  // vector<vector<int>>dp(n + 1, vector<int>(2));
+  int dp[n+1][2];
+  dp[0][0] = 0;
+  dp[0][1] = 0;
   for (int i = 1;i <= n;i++) {
     dp[i][true] = dp[i - 1][true] + (s[i - 1] == 'b');
     dp[i][false] = min(dp[i - 1][true], dp[i - 1][false]) + (s[i - 1] == 'a');
   }
   // dbg(dp)
-    return min(dp.back()[0], dp.back()[1]);
+    return min(dp[n][0], dp[n][1]);
 }
 };
