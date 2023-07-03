e62ff584f6115396fc5780e62a2baaa1001abf00
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2023-07-03 19:34:12 +0530

    Time: 23 ms (34.09%) | Memory: 7.3 MB (23.40%) - LeetSync

---
+++ print-in-order/print-in-order.cpp
@@ -0,0 +1,34 @@
+class Foo {
+public:
+    mutex m;
+    condition_variable cv;
+    int turn = 1;
+    Foo() {
+        
+    }
+
+    void first(function<void()> printFirst) {
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        unique_lock<mutex> lck(m);
+        printFirst();
+        turn++;
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond) {
+        unique_lock<mutex> lck(m);
+        // printSecond() outputs "second". Do not change or remove this line.
+        while(turn != 2)cv.wait(lck);
+        printSecond();
+        turn++;
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+         unique_lock<mutex> lck(m);
+        while(turn != 3)cv.wait(lck);
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+};
