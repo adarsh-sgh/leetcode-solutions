94557172544fa4408a235b002a1cc8a882311f14
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2023-08-01 00:55:05 +0530

    Time: 40 ms (100.00%) | Memory: 37.2 MB (60.00%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int countCompleteSubarrays(vector<int>& nums) {
+        int countCompleteSubarrays(vector<int>& nums) {
   map<int, int>m;
   for (auto&& x : nums) {
     m[x]++;
   }
   int distinct = m.size();
   m.clear();
-  int j = 0;
+  int i = 0;
   int n = nums.size();
   int ans = 0;
-  for (int i = 0;i < n;i++) {
-    m[nums[i]]++;
+
+  for (int j = 0;j < n;j++) {
+    m[nums[j]]++;
     while (m.size() >= distinct) {
-      m[nums[j]]--;
-      if (!m[nums[j]])m.erase(nums[j]);
-      j++;
-    }
-    if (j > 0 && m.size() == distinct - 1 && !m.count(nums[j - 1])) {
-      ans += j;
+      m[nums[i]]--;
+      if (!m[nums[i]])m.erase(nums[i]);
+      ans += nums.size() - j;
+      i++;
     }
   }
   return ans;
 }
+
 };
