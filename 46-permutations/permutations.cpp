5974b168d02c6a50e69b78382f585ab3dfceb3a5
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2023-08-02 09:30:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (60.28%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void pm(vector<vector<int>>&ans, vector<int>&nums,int index){
+      if(index >= nums.size()){
+        ans.push_back(nums);
+        return;
+      }
+      for(int i = index;i<nums.size();i++){
+        swap(nums[i],nums[index]);
+        pm(ans,nums,index+1);
+        swap(nums[i],nums[index]);
+      }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        pm(ans,nums,0);
+        return ans;
+    }
+};
