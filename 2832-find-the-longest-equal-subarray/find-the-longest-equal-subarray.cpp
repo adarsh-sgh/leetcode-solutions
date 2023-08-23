9f89aefbee82337dd0fd68d0186a115b3ea156c7
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2023-08-23 14:41:55 +0530

    Time: 746 ms (21.04%) | Memory: 446.8 MB (5.71%) - LeetSync

--- 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.cpp
+++ 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    int longestEqualSubarray(vector<int>& v, int k) {
-        int n = v.size();
-        vector<vector<int>> pref(n + 1);
-        for(int i = 0; i < n; ++i) {
-            pref[v[i]].push_back(i);
-        }
-
-        int ans = 0;
-        for(int ii = 1; ii <= n; ++ii) {
-            vector<int>& temp = pref[ii];
-            int i = 0, j = 0;
-            int elem = k;
-            int m = temp.size();
-            while(j < m) {
-                while(j + 1 < m && temp[j + 1] - temp[j] - 1 <= elem) {
-                    elem -= temp[j + 1] - temp[j] - 1;
-                    ++j;
-                }
-                ans = max(ans, j - i + 1);
-                if(i + 1 < m) {
-                    elem += temp[i + 1] - temp[i] - 1;
-                }
-                elem = min(elem, k);
-                ++i;
-                j = max(j, i);
-            }
-        }
-        return ans;
+#define dbg(...) 42;
+    int longestEqualSubarray(vector<int>& nums, int k) {
+  map<int, deque<int>>mp;
+  int ans = 0;
+  for (int i = 0;i < nums.size();i++) {
+    auto&& dq = mp[nums[i]];
+    dq.push_back(i);
+    while (dq.size()) {
+      int ji = i - dq.front() + 1;
+      if (ji - dq.size() <= k)break;
+      dq.pop_front();
     }
+    dbg(dq);
+    ans = max(ans, (int)dq.size());
+  }
+  return ans;
+}
 };
