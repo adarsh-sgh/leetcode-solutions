dc0d258adaac1edf9bf45b237403d41d1a5aa2a1
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2023-09-08 03:27:11 +0530

    Time: 520 ms (70.39%) | Memory: 83.4 MB (63.13%) - LeetSync

--- 2390-naming-a-company/naming-a-company.cpp
+++ 2390-naming-a-company/naming-a-company.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
-#define dbg(...) 42;
- long long distinctNames(vector<string>& ideas) {
-  unordered_set<string>se(ideas.begin(), ideas.end());
-  int n = ideas.size();
-  vector<unordered_set<char>>m(n);
-  
-  vector<vector<int>>mp(128, vector<int>(128, 0));
-  // map<char, map<char, int>>mp;
-  // map<char, int>cnt;
-  vector<int>cnt(128, 0);
-  for (int i = 0;i < n;i++) { 
-    cnt[ideas[i][0]]++;
-    for (char c = 'a';c <= 'z';c++) {
-      string conc = c + ideas[i].substr(1);
-      dbg(conc);
-      if (se.count(conc)) {
-        mp[c][ideas[i][0]]++;
-      }
-      else {
-        m[i].insert(c);
-      }
+    long long distinctNames(vector<string>& ideas) {
+        vector<unordered_set<string>>v(26);
+        for(auto &s:ideas){
+          v[s[0]-'a'].insert(s.substr(1));
+        }
+        long long res = 0;
+        for(int i = 0;i<26;i++){
+          for(int j = i + 1;j < 26;j++){
+            int l1 = 0, l2 = 0;
+            for(auto &p:v[i]){
+              if(!v[j].count(p))l1++;
+            }
+             for(auto &p:v[j]){
+              if(!v[i].count(p))l2++;
+            }
+            res += l1 * l2;
+          }
+        }
+        return 2 * res;
     }
-  }
-  long long ans = 0;
-  for (int i = 0;i < n;i++) {
-    for (auto&& x : m[i]) {
-      ans += cnt[x] - mp[ideas[i][0]][x];
-    }
-  }
-  return ans;
-}
 };
