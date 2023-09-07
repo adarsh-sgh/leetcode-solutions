e82fc06d93894cb3333f7ac094228165083f393f
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2023-09-08 04:47:00 +0530

    Time: 1991 ms (5.01%) | Memory: 415.3 MB (5.01%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
-//copied
-    int solve(vector<vector<int>>&dp, vector<vector<int>>& events, int n, int pos, int k)
-{
-        int i;
-        
-        if(pos >= n || k == 0)
-            return 0;
-        
-        if(dp[pos][k] != -1)
-            return dp[pos][k];
-        
-        // finding the next event which we can attend
-        for(i = pos + 1; i < n; i++)
-            if(events[i][0] > events[pos][1])
-                break;
-        
-       return dp[pos][k] = max(solve(dp, events, n, pos+1, k), events[pos][2] + solve(dp, events, n, i, k-1));
-
-    }
-    
-    int maxValue(vector<vector<int>>& events, int k) 
-    {
+    int maxValue(vector<vector<int>>& events, int k) {
         int n = events.size();
-        sort(events.begin(), events.end());
-        
-        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
-        
-        return solve(dp, events, n, 0, k);
+        sort(events.begin(), events.end(), [](auto a, auto b) {
+        return a[1] < b[1];
+         });
+         vector<int>v1(n);
+        for(int i = 0;i<n;i++)v1[i] = events[i][1];
+        vector<vector<int>>dp(n+1,vector<int>(k+1));
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j <= k;j++){
+                // int ip = i;
+                auto ip = lower_bound(v1.begin(),v1.begin() + i-1,events[i-1][0]) - v1.begin();
+                // while(ip && events[ip-1][1] >= events[i-1][0])ip--;
+                dp[i][j] = max(dp[i-1][j],events[i-1][2] + dp[ip][j-1]);
+            }
+        }
+        return *max_element(dp[n].begin(),dp[n].end());
     }
 };
