ff9555a7011461a53143caf5fb38479853705944
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2023-09-24 03:16:04 +0530

    Time: 378 ms (74.11%) | Memory: 175.8 MB (45.43%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,43 +1,56 @@
 class LRUCache {
 public:
-    map<int,pair<int,int>>m;
-    map<int,int>m2;
-    int t = 0;
-    int c;
-    LRUCache(int capacity) {
-        c = capacity;
-    }
-    
-    int get(int key) {
-        if(!m.count(key)) return -1;
-        m2.erase(m[key].first);
-        m[key].first = t;
-        m2[t] = key;
-        t++;
-        return m[key].second;
-    }
-    
-    void put(int key, int value) {
-        if(m.size() == c && !m.count(key)){
-            int lruKey = m2.begin()->second;
-            // cout<<lruKey<<" ";
-            m2.erase(m2.begin());
-            m.erase(lruKey);
-        }
-        if(m.count(key)){
-            int time = m[key].first;
-            m2.erase(time);
-        }
-        m[key] = {t,value};
-        
-        m2[t] = key;
-        t++;
-    }
+struct DLL {
+  int key;
+  int val;
+  DLL* next;
+  DLL* prev;
+  DLL(int k, int v, DLL* p, DLL* n) {
+    key = k;
+    val = v;
+    next = n;
+    prev = p;
+  }
 };
+  int cap;
+  DLL* phead = new DLL(-1, -1, nullptr, nullptr);
+  DLL* ntail = new DLL(-1, -1, phead, nullptr);
+  unordered_map<int, DLL*>mp;
+  LRUCache(int capacity) {
+    phead->next = ntail;
+    cap = capacity;
+  }
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
+  int get(int key) {
+    if (!mp.count(key))return -1;
+    auto node = mp[key];
+    del(node);
+    node->prev = phead;
+    node->next = phead->next;
+    phead->next->prev = node;
+    phead->next = node;
+    return node->val;
+  }
+
+  void del(DLL* node) {
+    // mp.erase(node->key);
+    DLL* nxt = node->next;
+    DLL* prv = node->prev;
+    nxt->prev = prv;
+    prv->next = nxt;
+  }
+  void put(int key, int value) {
+    if(mp.count(key)){
+        del(mp[key]);
+        mp.erase(key);
+    }
+    if (mp.size() == cap ) {
+      mp.erase(ntail->prev->key);
+      del(ntail->prev);
+    }
+    DLL* phn = phead->next;
+    phead->next = new DLL(key, value, phead, phn);
+    phn->prev = phead->next;
+    mp[key] = phead->next;
+  }
+};
