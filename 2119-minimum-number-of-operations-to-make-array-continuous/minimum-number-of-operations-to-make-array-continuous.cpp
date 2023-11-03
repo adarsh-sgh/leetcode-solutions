57d45308438de85c918f2908f7684b767c7a34bd
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2023-11-03 09:42:24 +0530

    Time: 238 ms (40.65%) | Memory: 64.7 MB (60.64%) - LeetSync

--- 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int N = nums.size();
-        auto ip = unique(nums.begin(),nums.end());
-        nums.resize(distance(nums.begin(),ip));
-        long  ops = 1e6;
-        int n = nums.size();
-        for(int i = 0; i < n; i++){
-            //if the seq starts at nums[i]
-            // nums[i], nums[i] + n - 1
-            auto x = lower_bound(nums.begin(),nums.end(),nums[i]) - 1;
-            auto y = upper_bound(nums.begin(),nums.end(),nums[i] + N - 1) - 1;
-            ops = min(ops,N - (y - x));
-        }
-        return ops;
+       sort(nums.begin(),nums.end());
+       long long sz = nums.size();
+       auto it = unique(nums.begin(),nums.end());
+       nums.erase(it,nums.end());
+       int ans = INT_MAX;
+       for(int i = 0;i<nums.size();i++){
+            // cnt of nums in range nums[i] to nums[i] + nums[i] + nums.size() - 1;
+            auto lb = lower_bound(nums.begin(),nums.end(),nums[i]);
+            auto ub = upper_bound(nums.begin(),nums.end(),nums[i] + sz -1);
+            ans = min(1ll *ans,sz - (ub-lb));
+       }
+       return ans;
     }
 };
