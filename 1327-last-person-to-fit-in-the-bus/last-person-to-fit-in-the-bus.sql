cf6edbdbc85a3e0fd507ffa2e38b7116d669e835
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2023-11-13 18:20:52 +0530

    Time: 835 ms (59.71%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,3 +1,3 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT person_name from
 (SELECT person_name, (select sum(weight) from Queue as q2 where q2.turn <= q1.turn ) as csum from Queue as q1 ORDER BY csum DESC ) as t2 where csum <= 1000 LIMIT 1;
