18ac4c7365af01a698446899cb5531255e3feeba
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2024-09-03 02:28:03 +0530

    Time: 11 ms (91.22%) | Memory: 18.1 MB (27.78%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,28 +1,58 @@
 class Solution {
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
-        vector<int>indegree(n);
-        vector<vector<int>>adj(n);
-        for(auto &p:prerequisites){
-            adj[p[1]].push_back(p[0]);
-            indegree[p[0]]++;
+// from course schedule 1
+ bool canFinish(int n, vector<vector<int>>& prerequisites) {
+     // bi -> ai
+     // 0, 1, 2 tricolor dfs
+    vector<vector<int>>adj(n);
+    for(auto &e:prerequisites){
+        adj[e[1]].push_back(e[0]);
+    }
+    vector<int>vis(n);
+     function<int(int)>hasLoop = [&](int x)->int{
+        if(vis[x]==1) return true;
+        if(vis[x]==2) return false;
+        vis[x] = 1;
+        for(auto &a:adj[x]){
+            if(hasLoop(a)) return true;
         }
+        vis[x] = 2;
+        return false;
+     };
+     for(int i = 0;i<n;i++){
+        if(hasLoop(i)) return false;
+     }
+     return true;
+    }
+
+    vector<int> findOrder(int n, vector<vector<int>>& prq) {
+       if(!canFinish(n,prq)) return {};
+       vector<vector<int>>adj(n);
+        for(auto &e:prq){
+            adj[e[1]].push_back(e[0]);
+        }
+        vector<int>indegree(n);
+        for(auto &e:prq)indegree[e[0]]++;
+        vector<int>ans;
         queue<int>q;
         for(int i = 0;i<n;i++){
-            if(indegree[i]==0)q.push(i);
-        }
-        vector<int>topo;
-        while(q.size()){
-            auto t = q.front();
-            q.pop();
-            topo.push_back(t);
-            for(auto &a:adj[t]){
-                indegree[a]--;
-                if(indegree[a]==0)q.push(a);
+            if(indegree[i] == 0){
+                q.push(i);
+                ans.push_back(i);
             }
         }
-        if(topo.size()< n) return {};
-        return topo;
+        while(q.size()){
+            auto f = q.front();
+            q.pop();
+            for(auto &a:adj[f]){
+                indegree[a]--;
+                if(indegree[a]==0){
+                    ans.push_back(a);
+                    q.push(a);
+                }
+            }
 
+        }
+        return ans;
     }
 };
