573fbc310a88e0024d16857b72460308b38abc78
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2024-10-20 15:47:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (91.40%) - LeetSync

--- 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
-   bool parseBoolExpr(string expression) {
-  stack<char>st;
-  for (auto&& c : expression) {
-    if (c == ')') {
-      bool hasT = false, hasF = false;
-      while (st.top() == 't' || st.top() == 'f') {
-        hasT |= st.top() == 't';
-        hasF |= st.top() == 'f';
-        st.pop();
-      }
-      auto op = st.top();
-      st.pop();
-      char res;
-      if (op == '&') {
-        if (hasF)res = 'f';
-        else res = 't';
-      }
-      else if (op == '|') {
-        if (hasT) res = 't';
-        else res = 'f';
-      }else if (op=='!'){
-          if(hasT) res ='f';
-          else res = 't';
-      }
-      st.push(res);
+    bool parseBoolExpr(string expression) {
+        vector<char> vops;
+        vector<char>tfb;
+        set<char> ops = {'|','&','!'};
+        for(auto &c:expression){
+            if(c==',')continue;
+            if(c != ')'){
+                if(ops.count(c)){
+                   vops.push_back(c); 
+                }else{
+                    tfb.push_back(c);
+                }
+            }else{
+                char tb = tfb.back();tfb.pop_back();
+                bool x = tb == 't';
+                while(tfb.back() != '('){
+                    auto curr = tfb.back();
+                    tfb.pop_back();
+                    auto ob = vops.back();
+                    if(ob == '&'){
+                       x = x & (curr == 't');
+                    }else{
+                        x = x | (curr == 't');
+                    }
+                }
+                tfb.pop_back();// remove (
+                if(vops.back() == '!'){
+                    x = !x;
+                }
+                vops.pop_back();
+                tfb.push_back(x ? 't' : 'f');
+            }
+        }
+        return tfb[0] == 't';
     }
-    else if (c != ',' && c != '(') {
-      st.push(c);
-    }
-  }
-  return st.top() == 't';
-}
 };
