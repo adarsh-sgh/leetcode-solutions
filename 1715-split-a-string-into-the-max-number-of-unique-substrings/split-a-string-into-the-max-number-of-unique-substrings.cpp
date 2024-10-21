d8da23aaea660c99c4264ba4d8b92792f418a555
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2024-10-21 22:12:07 +0530

    Time: 266 ms (42.98%) | Memory: 64.7 MB (38.89%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
+    set<string>se;
     int ans = 0;
-    void ms(string &s,int i, map<string,int>&mp){
-       if(i == s.size()) ans = max(ans, (int)mp.size());
-       string curr; 
-       for(int j = i;j<s.size();j++){
-        curr += s[j];
-        if(!mp.count(curr)){
-            mp[curr]++;
-            ms(s,j+1,mp);
-            mp.erase(curr);
+    string s;
+    void dfs(int i){
+        if(i == s.size()){
+            ans = max(ans, (int)se.size());
         }
-       } 
+        string curr;
+        for(int j = i;j<s.size();j++){
+            curr += s[j];
+            if(se.count(curr))continue;
+            se.insert(curr);
+            dfs(j+1);
+            se.erase(curr);
+        }
     }
-    int maxUniqueSplit(string s) {
-       map<string,int>mp;
-       ms(s,0,mp);
-       return ans; 
+    int maxUniqueSplit(string S) {
+       s = S;
+        dfs(0);
+        return ans;
     }
 };
