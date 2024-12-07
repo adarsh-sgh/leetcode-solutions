0ad832dc5e1f6e53b6b2136e4c0b9bea1db9aa8c
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2024-12-07 13:19:42 +0530

    Time: 4 ms (31.32%) | Memory: 9.6 MB (34.50%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-int mod = 1e9+7;
     int distinctSubseqII(string s) {
-  vector<int>prv(128);
-  int n = s.size();
-  //dp[i] -> distinct subseqs ending at i not counted before
-  vector<int>dp(n);
-  dp[0] = 1;
-  vector<int>dpp(n+1);
-  dpp[1] = 1;
-  for (int i = 1;i < s.size();i++) {
-    char c = s[i];
-    int pi = prv[s[i]];
-    if (s[pi] != s[i])dp[i] = 1;
-    // for (int j = pi;j < i;j++) {
-    //   dp[i] += dp[j];
-    //   dp[i] %= mod;
-    // }
-    dp[i] += (mod  + dpp[i] - dpp[pi]) % mod;
-    dpp[i+1] = (dpp[i] + dp[i]) % mod;
-    prv[s[i]] = i;
-  }
-//   dbg(dp);
-  int ans = 0;
-  for (int i = 0;i < n;i++){
-    ans += dp[i];
-    ans %= mod;
+       int n = s.size();
+       vector<long long>dp(n+1);
+       dp[0] = 1;
+       int mod = 1e9 + 7;
+       int ans = 0;
+       for(int i = 1;i<=n;i++){
+            for(int j = i-1;j>=0;j--){
+               dp[i] += dp[j]; 
+               dp[i] %= mod;
+               if(j == 0 || s[j-1] == s[i-1])break;
+            } 
+            ans += dp[i];
+            ans %= mod;
+       } 
+       return ans;
+
+
     }
-  return ans;
-}
 };
