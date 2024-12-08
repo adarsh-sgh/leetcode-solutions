bedca055522f872612bed878782710a0370a202c
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2024-12-08 23:04:46 +0530

    Time: 2385 ms (5.40%) | Memory: 564.3 MB (5.57%) - LeetSync

---
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& events) {
+       // sort by end and start times 
+       vector<vector<int>>vc = events;
+       sort(events.begin(),events.end(), [&](auto a, auto b) {
+        return a[1] < b[1];
+       });
+       sort(vc.begin(),vc.end());
+       // max val of an interval with start > x, this start will keep dec. 
+       int j = vc.size() - 1;
+       int n = vc.size();
+       int ans = 0;
+       int mx = 0;
+        for(int i = n - 1;i>=0;i--){
+            int e = events[i][1];
+            while(j >= 0 && vc[j][0] > e){
+                mx = max(mx, vc[j][2]);
+                j--;
+                };
+            ans = max(ans, mx + events[i][2]);
+        }
+        return ans;
+    }
+};
