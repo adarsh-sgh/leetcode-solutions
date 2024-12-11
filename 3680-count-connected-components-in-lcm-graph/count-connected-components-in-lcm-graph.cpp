e434ec1f65db7d5f8c0b38b9a3307dbd79c10c0c
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2024-12-11 09:01:10 +0530

    Time: 118 ms (83.74%) | Memory: 69.3 MB (95.20%) - LeetSync

--- 3680-count-connected-components-in-lcm-graph/count-connected-components-in-lcm-graph.cpp
+++ 3680-count-connected-components-in-lcm-graph/count-connected-components-in-lcm-graph.cpp
@@ -12,6 +12,16 @@ public:
 // component count -> dsu 
 // hint: Connect a number to all its multiples less than threshold
 // introducing extra nodes is not a problem, since we just need component count 
+
+//! tc calculation is tricky, copied from discussion:
+
+//  Time complexity: Since the elements in nums are unique. So, worst case would be when all the elements in the nums array are like this 2, 3, 4, 5, 6, ... k. So, we can say that in worst case the complexity of algorithm would be:
+// th/1+th/2+th/3+th/4+th/5+...+th/k=th∗log(th) (because the max value of k could be th)
+// O(max(n,th∗log(th)))
+// Here th is threshold
+// Reference: Sum of Harmonic Series
+
+
     int countComponents(vector<int>& nums, int t) {
        sort(nums.begin(),nums.end());
        p.resize(t+1);
