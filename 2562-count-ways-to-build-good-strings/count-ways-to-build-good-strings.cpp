3c51f006853f4c41623d153d6ba4b697ee19bc55
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2024-12-30 20:48:33 +0530

    Time: 1291 ms (5.09%) | Memory: 11.6 MB (75.54%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int mod = 1e9 + 7;
-    int countGoodStrings(int low, int high, int z, int o) {
-        vector<vector<long long>>dp(high + 1, vector<long long>(2));
-        dp[0][0] = 1;
-        long long ans = 0;
-        for(int i = 1;i<=high;i++){
-            if(i-z>=0) dp[i][0] = dp[i-z][0] + dp[i-z][1];
-            if(i-o>=0) dp[i][1] = dp[i-o][0] + dp[i-o][1];
-            dp[i][0] %= mod;
-            dp[i][1] %= mod;
-            if(i>= low && i <= high){
-                ans += dp[i][0] + dp[i][1];
-                ans %= mod;
-            }
-            
-        }
-        return ans;
-
+    int countGoodStrings(int low, int high, int zero, int one) {
+       vector<int>dp(high+1);
+       dp[0] = 1;
+        int mod = 1e9 + 7;
+       for(int i = 1;i<=high;i++){
+            // put 1 now 
+            if(i >= one)dp[i] += dp[i - one];
+            // put 0 now 
+            if(i >= zero)dp[i] += dp[i - zero];
+            dp[i] %= mod;
+       }
+       for(auto &x:dp)cout<<x<<' ';
+       return accumulate(dp.begin() + low, dp.end(), 0ll) % mod;
     }
 };
