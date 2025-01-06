f60fa320dd03c7df180a78e056c00b8ba88b75c9
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2025-01-06 20:22:15 +0530

    Time: 2 ms (66.27%) | Memory: 13.1 MB (6.09%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        int n = boxes.size();
-        vector<int>v(n);
-        int sum = 0,cnt = 0;
-        for(int i = 0;i<n;i++){
-            v[i]+= sum;
-            if(boxes[i]-'0')cnt++;
-            sum+=cnt;
-        }
-        sum = 0;cnt = 0; 
-        for(int i = n-1;i>=0;i--){
-            v[i]+= sum;
-            if(boxes[i]-'0')cnt++;
-            sum+=cnt;
-        }
-        return v;
+       // ans[i] = sum((j - i) * boxes[j])
+       int n = boxes.size();
+       // sum, cnt
+       vector<array<long long,2>>pf(n+1);
+       for(int i = 0;i<n;i++){
+        bool x = boxes[i] == '1';
+        pf[i+1] = {pf[i][0] + x*i, pf[i][1] + x};
+       }
+
+       vector<int>ans(n);
+       for(int i = 0;i < n;i++){
+         auto [sp, cp] = pf[i];
+         auto ss = pf.back()[0] - sp;
+         auto cs = pf.back()[1] - cp;
+         ans[i] = abs(sp - cp * i) + abs(ss - cs * i);
+       }
+       return ans;
+      
     }
+
 };
