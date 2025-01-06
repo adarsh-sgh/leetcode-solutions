bbaf40e2c3177f63e861c322262fe92c1e126391
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2025-01-06 21:06:26 +0530

    Time: 8 ms (17.19%) | Memory: 16.9 MB (62.84%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-   static bool cmp(string a, string b){
-    return a+b<b+a;
-}
+    string largestNumber(vector<int>& nums) {
+       vector<string>v;
+       for(auto &x:nums){
+        v.push_back(to_string(x));
+       } 
+       sort(v.begin(),v.end(),[&](auto &x, auto &y){
+            string n1 = x + y, n2 = y + x;
+            // number bada => ya to length badi (without leading zero) (len is same here)
+            // ya fir first no. jispar differ karte wo bada 
+            for(int i = 0;i<n1.size();i++){
+                if(n1[i] == n2[i]) continue;
+                return n1[i]  > n2[i];
+            }
+            return false;
+            // return stoll(x + y) > stoll(y + x);
+       });
 
-  string largestNumber(vector<int>& nums) {
-        vector<string> s(nums.size());
-        for(int i =0;i<nums.size();i++){
-          s[i] = to_string(nums[i]);
-        }
-        sort(s.begin(),s.end(),cmp);
-        string ans;
-        for(int i = s.size()-1;i>=0;i--){
-          ans+=s[i];
-        }
-      if(ans[0]=='0')
-          return "0";
-      return ans;
+       string ans;
+       if(v[0] == "0") return "0";
+       for(auto &x:v){
+        ans+= x;
+       }
+       // trim leading zeroes
+       return ans;
     }
 };
