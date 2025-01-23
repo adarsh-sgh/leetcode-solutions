93c244a78e83784db5623921e46121ebe42e9007
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2025-01-23 16:46:00 +0530

    Time: 7 ms (92.56%) | Memory: 35.1 MB (35.63%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-    int shipWithinDays(vector<int>& weights, int days) {
-       auto good = [&](int c){
-          int cnt = 0;
-          int curr = 0;
-          for(auto &w:weights){
-            if(curr + w > c){
-                curr = 0;
-                cnt++;
-            }
-            curr += w; 
-          }
-          if(curr){
-            cnt++;
-          }
-          return cnt <= days;
-       }; 
-       int l = *max_element(weights.begin(),weights.end());
-       int r = accumulate(weights.begin(), weights.end(),0ll);
-       while(l < r){
-         int m = (l + r)>> 1;
-         if(good(m)){
-            r = m;
-         }else{
+bool canCarry(int limit, vector<int> &weights, int trips) {
+    int currWeight = 0;
+    for(int i = 0; i < weights.size(); i++) {
+        if (weights[i] + currWeight <= limit) {
+            currWeight += weights[i];
+        } else {
+            trips--;
+            currWeight = weights[i];
+        }
+    }
+    return trips > 0;
+}
+
+int minWeight(vector<int> &weights, int trips) {
+    int l = 0, h = 0, m, res;
+    for (auto &w : weights) {
+        l = max(w, l);
+        h += w;
+    }
+    while (l <= h) {
+        m = l + (h-l) / 2;
+        if (canCarry(m, weights, trips)) {
+            h = m - 1;
+            res = m;
+        } else {
             l = m + 1;
-         }
-       }
-       return l;
+        }
+    }
+    return res;
+}
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        return minWeight(weights, days);
     }
 };
