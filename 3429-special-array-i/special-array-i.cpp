cb36e49ed66664bc560b28cac609e099fb501349
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2025-02-01 14:44:31 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (2.79%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+       for(int i = 1;i < nums.size();i++){
+        if(~(nums[i] ^ nums[i-1]) & 1){
+            return false;
+        }
+       } 
+       return true;
+    }
+};
