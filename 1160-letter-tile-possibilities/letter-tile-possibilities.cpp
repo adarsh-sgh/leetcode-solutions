1095964cf05dd94b47d93db0d8c71a19635c2a1e
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2025-02-17 09:28:39 +0530

    Time: 40 ms (36.47%) | Memory: 16.9 MB (46.94%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int numTilePossibilities(string tiles) {
         // n = 7, n! = 5040, 2^n = 128 
+        // find all comb. using bm then all perms of them
+        // better approach: see first submission of june 2
         set<string> se;
         int n = tiles.size();
+         sort(tiles.begin(), tiles.end());
         for(int bm = 1;bm < (1<<n);bm++){
            string s;
            for(int i = 0;i < n;i++){
             if(bm >> i & 1)s+=tiles[i];
            } 
           if(se.count(s))continue;
-          sort(s.begin(), s.end());
+         
           do{
             se.insert(s);
           }while(next_permutation(s.begin(), s.end()));
