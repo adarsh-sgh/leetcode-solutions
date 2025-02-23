c4dc4938f22128574beac1288634a7db01317c11
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2025-02-23 21:26:51 +0530

    Time: 0 ms (100.00%) | Memory: 28.8 MB (10.59%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,39 +1,50 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+
  */
 class Solution {
 public:
-    vector<int>pre,post;
-    map<int,int>postm;
-    TreeNode *cfpp(int i1,int j1,int i2,int j2){
-        if(i1 > j1) return nullptr;
-        assert(j1-i1==j2-i2);
-        assert(pre[i1]==post[j2]);
-        TreeNode *r = new TreeNode(pre[i1]);
-        i1++;
-        j2--;
-        if(i1>j1)return r;
-        int r2 = postm[pre[i1]];
-        int r1 = i1 + r2 - i2;
-        r->left = cfpp(i1,r1,i2,r2);
-        r->right = cfpp(r1+1,j1,r2+1,j2);
-        return r;
-    }
+   #define dbg(...) 42;
+    vector<int> pre, pos;
+    map<int, int> postMp;
+
+    // Make a tree considering i1, j1 of preorder and i2, j2 of postorder
+   TreeNode* dfs(int i1, int j1, int i2, int j2) {
+    if (i1 > j1 || i1 >= pre.size()|| i1 < 0) return nullptr;
+    // Use .at() for bounds-checked access
+  dbg(i1, j1, i2, j2);
+  assert(j1 - i1 == j2 - i2);
+    TreeNode* root = new TreeNode(pre.at(i1));
+    if(i1 == j1) return root;
+    // Now decide how to split the ranges between left and right subtrees
+    // i1 + 1 will be the root of the left subtree
+    // This should be the last index of postorder sent to the left
+    // Also, the size of segments sent to left and right will be the same
+    // i.e., j1 - i1 == j2 - i2 always
+    int i1l = i1 + 1;
+    int j2l = postMp.at(pre.at(i1l)); // Use .at() for bounds-checked access
+
+    // Make j1 - i1 == j2 - i2
+    // i1 and j2 are consumed here
+    int j1l = i1l + j2l - i2;
+
+    // Recursively build the left and right subtrees
+    root->left = dfs(i1l, j1l, i2, j2l);
+    root->right = dfs(j1l + 1, j1, j2l + 1, j2 - 1);
+
+    return root;
+}
 
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
         pre = preorder;
-        post = postorder;
-        for(int i = 0;i<post.size();i++){
-            postm[post[i]] = i;
+        pos = postorder;
+
+        // Populate the postorder map using .at() for bounds-checked access
+        for (int i = 0; i < postorder.size(); i++) {
+            postMp[postorder.at(i)] = i;
         }
-        return cfpp(0,preorder.size()-1,0,postorder.size()-1);
+
+        // Start the DFS with the full ranges of preorder and postorder
+        return dfs(0, preorder.size() - 1, 0, postorder.size() - 1);
     }
 };
