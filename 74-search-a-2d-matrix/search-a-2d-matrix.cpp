6711ae0ce4e2124e1334f7185bf2c3ab9c5f2d0d
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2025-04-06 19:14:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.88%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-   
+     int m, n;
+    int id(int x,int y){
+        return y + x * n;
+    }
+    // x, y
+    array<int,2> coord(int id){
+        int x = id / n;
+        int y = id % n;
+        return {x,y};
+    }
+    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size(), n = matrix[0].size();
-        auto index = [&](int x,int y)->int{
-            return x * n + y;
-        };
-        auto at = [&](int i)->int{
-            int x = i / n;
-            int y = i % n;
-            return matrix[x][y];
-        };
-        int l = 0, r = index(m-1,n-1);
+       // low = 0, 0 0
+       // 2, 3 => 11 => 
+       // high = n, m
+        m = matrix.size();n = matrix[0].size(); 
+        int l = 0, r = id(m-1, n-1);
         while(l < r){
-            int m = (l + r) >> 1;
-            if(at(m) >= target){
+            int m = (l + r)/2;
+            auto [x,y] = coord(m);
+            if(matrix[x][y] >= target){
                 r = m;
             }else{
                 l = m + 1;
             }
         }
-        return at(l)==target;
+        auto [x,y] = coord(l);
+        return matrix[x][y] == target;
     }
 };
