c52b62fbc4b2a6a984def95c32fa3a543e8a83df
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2025-04-14 01:39:39 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (33.88%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l = 0, r = n-1;
-        while(l<r){
-            if(nums[0] < nums.back())break;
-            int mid = (l + r)/2;
-            if(nums[mid] < nums[0]) r = mid;
-            else l = mid + 1;
-        }
+// second half    first half
+// 4,5,6,7,       0,1,2
+// target -> something in first half
+// v[i] >= target
+// f f t t f f f
+// 
+// f f t t t t t, target = 6
+// bool inFirst = v.back() < target;
+// (v[i] >= target) || (v[i] <= v.back());
 
-        if(target <= nums.back()){
-            r = n -1;
-        }else{
-            r = l -1;
-            l = 0;
-        }
-        cout<<l<<' '<<r;
-        auto in = lower_bound(nums.begin() + l, nums.begin() + r + 1,target) - nums.begin();
-        if(nums[in]==target) return in;
+// 4,5,6,7,       0,1,2
+// target = 1
+// v[i] >= target && v[i] <= v.back()
+//    t t t t t t t
+
+// 0 1 2 4 5 6 7, target = 4 
+// f f f t t t t => 
+// v[i] >= target
+bool good (vector<int>&v, int i, int t){
+    bool tfh = t <= v.back();
+    if(tfh){
+        return v[i] >= t && v[i] <= v.back();
+    }else{
+        return v[i] >= t || v[i] <= v.back();
+    }
+}
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while(l < r){
+            int m = (l + r)/2;
+            if(good(nums,m, target)){
+                r = m;
+            }else{
+                l = m + 1;
+            }
+        }        
+        if(nums[l] == target) return l;
         return -1;
     }
 };
