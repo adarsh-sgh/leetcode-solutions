ac5bb4dcdff16f4f6a0f42ada2193f52fb078949
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2025-04-26 04:05:41 +0530

    Time: 1618 ms (5.76%) | Memory: 132.2 MB (5.23%) - LeetSync

--- 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
+
     long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
-  int n = nums.size();
-  vector<int>p(n + 1);
-  for (int i = 0; i < n; i++) {
-    p[i + 1] = p[i] + (nums[i] % modulo == k);
-  }
-  map<int, int>mp;
-  long long ans = 0;
-//   dbg(p);
-  for (int i = 0;i < n + 1;i++) {
-    auto cur = p[i] % modulo;
-    if (cur - k < 0) ans += mp[(-k + cur + modulo) % modulo];
-    else ans += mp[(-k + cur) % modulo];
-    mp[cur]++;
-  }
-  return ans;
-}
+        int n = nums.size();
+        vector<int>ok(n);
+        for(int i = 0;i< n;i++) ok[i] = (nums[i] % modulo) == k;
+        vector<int>p(n+1);
+        for(int i = 1;i<=n;i++){
+            p[i] = (p[i-1] + ok[i - 1]) % modulo;
+        }
+        for(auto &x:p)cout<<x<<' ';
+        // at py find px such that
+        // py - px % m == k
+        // (m + py % m  - px % m) % m = k
+        // px  = py  - k  or m + py - k = px
+        // py = (k + px) % m wale numbers
+        map<int,int>mp;
+        long long ans = 0;
+        for(int i = 0;i< p.size();i++){
+            ans += mp[(p[i] - k + modulo) % modulo];
+            mp[p[i]]++;
+        }
+        return ans;
+    }
+
 };
