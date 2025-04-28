8ad081a7c9ba088718dbe581f73d4f226dfdf403
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2025-04-28 21:23:11 +0530

    Time: 83 ms (5.19%) | Memory: 108.1 MB (7.26%) - LeetSync

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.cpp
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, long long k) {
-        long long sum = 0,ans = 0;
-        int j = 0;
-        for(int i = 0;i<nums.size();i++){
-            sum += nums[i];
-            while(sum * (i - j + 1) >= k){
-                sum -= nums[j];
-                j++;
+      int n = nums.size();
+      // subarrays ending at i with score < k can be found with bs
+      vector<long long>p(n+1);
+      for(int i = 0;i< n;i++)p[i+1] = p[i] + nums[i];
+      // i to j subarray ka score < k hai kya ?
+      auto good = [&](int i, int j){
+        if(j >= n) return false;
+        return (p[j+1] - p[i])*1ll * (j - i + 1) < k;
+      };
+
+      long long ans = 0;
+      for(int i = 0;i < nums.size();i++){
+        // f f f, t t t 
+        int l = 0, r = i;
+        while(l < r){
+            int m = (l + r)/2;
+            if(good(m, i)){
+                r = m;
+            }else{
+                l = m + 1;
             }
-            ans += i - j + 1;
         }
-        return ans;
+        if(good(l, i)){
+          ans += i - l + 1;  
+        }
+      }    
+      return ans;
     }
 };
