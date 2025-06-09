87773990693bc01b60a0f9a2562a1c72839cc9f4
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2025-06-09 19:12:57 +0530

    Time: 17 ms (19.00%) | Memory: 60.7 MB (6.07%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
-    // ans min = 10, max = 55
-    // []
-    int d;
-    bool good(vector<int>&weights, int  weightLimit){
-       // arr [no, no , no ,no ..... no, yes, yes....yes] 
-
-       // find index of first yes
-       // 10 -> no
-       // 11 -> no ....  
-       //   55  -> yes
-       int days = 1;
-       int curr = 0;
-       for(auto &w:weights){
-        if(curr + w > weightLimit){
-            days++;
-            curr = 0;
-        }
-        curr += w; 
-       }
-       return days <= d;
+int tripsCount(int cap, vector<int>weights){
+  int load = 0;
+  int trips =1;
+  for(int i =0;i< weights.size();i++){
+    if(load+weights[i]>cap){
+      trips+=1;
+      load =weights[i];
     }
-
-    int shipWithinDays(vector<int>& weights, int days) {
-        d = days;
-        int l  = 0, r = 0;
-        for(auto &w:weights){
-            r += w;
-            l = max(w, l);
-        }
-
-        while(l < r){
-            int m = (l + r) / 2;
-            if(good(weights,m)){
-                r = m;
-            }else{
-                l = m + 1;
-            }
-        }
-        return l;
+    else{
+      load+=weights[i];
     }
+  }
+  return trips;
+}
+
+
+int shipWithinDays(vector<int>& weights, int trips) 
+{
+    // vector<int>weights = {3,2,2,4,1,4};
+    // int trips = 3;
+    int sum = 0;
+    int maxi = INT_MIN;
+    for(int i =0;i<weights.size();i++){
+      sum+=weights[i];
+      if(maxi<weights[i]){
+        maxi = weights[i];
+      }
+    }
+    while(maxi<=sum){
+      int mid = (maxi+sum)/2;
+      int req_trips = tripsCount(mid, weights);
+      if (req_trips<= trips){
+        sum = mid -1;
+      }
+      else{
+        maxi = mid +1;
+      }
+    }
+    return maxi;
+
+}
 };
