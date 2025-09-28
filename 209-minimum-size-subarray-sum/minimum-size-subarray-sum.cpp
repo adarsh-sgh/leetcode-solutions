3222c791dc55d97d5c5297bfe88d9eff62ccdfac
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2025-09-29 00:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (71.70%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-     int i = 0;
-     int sum = 0;
-     int ans = nums.size() + 1;
-     for(int j = 0;j < nums.size();j++){
-       sum += nums[j];
-       while(sum >= target){
-        ans = min(ans, j - i + 1);
-        sum -= nums[i];
-        i++;
-       } 
-     }   
-     if(ans > nums.size()) return 0;
-     return ans;
+       // smallest length with sum >= target
+       int i = 0, csum = 0, ans = nums.size() + 1;
+       for(int j = 0;j < nums.size();j++){
+        csum+= nums[j];
+        while(csum >= target){
+            ans = min(ans, j - i + 1);
+            csum -= nums[i];
+            i++;
+        }
+       }
+       if(ans==nums.size() + 1)return 0;
+       return ans;
     }
 };
