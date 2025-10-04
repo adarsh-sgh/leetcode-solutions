c13ea9627a2190e9b29171aa7f7c8bacb459ffda
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2025-10-04 17:55:16 +0530

    Time: 0 ms (100.00%) | Memory: 63 MB (12.56%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-     int i = 0, j = height.size() - 1;
-     int ans = 0;
-     while(i < j){
+       int i = 0, j = height.size() - 1;
+       int ans = 0;
+       while(i < j){
         ans = max(ans, min(height[i], height[j]) * (j - i));
-        // if i, j is not the optimal ans
-        // then chote wale ko to for sure move karna padega
-        // bade wale ko baad mein dekh lenge 
         if(height[i] < height[j]){
-           i++;
+            i++;
         }else{
             j--;
         }
-     } 
-     return ans;
+       } 
+       return ans;
     }
 };
