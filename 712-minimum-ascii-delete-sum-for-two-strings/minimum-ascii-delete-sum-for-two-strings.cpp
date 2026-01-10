57c39df56e5e7a87c75a83b4813f2e0533d2b6ff
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2026-01-10 19:54:29 +0530

    Time: 14 ms (78.67%) | Memory: 18.4 MB (23.97%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-#define dbg(...) 42;
-string s,t;
-vector<vector<int>>dp;
-int mds(int i, int j) {
-  if (i == s.size() || j == t.size()) {
-    int del = 0;
-    while (i < s.size()) del += s[i++];
-    while (j < t.size()) del += t[j++];
-    return dp[i][j] = del;
-  }
-  if(dp[i][j] != -1) return dp[i][j];
-
-  if (s[i] == t[j]) {
-    return dp[i][j] =  mds(i + 1, j + 1);
-  }
-  else {
-    return dp[i][j] =  min(s[i] + mds(i + 1, j), t[j] + mds(i, j + 1));
-  }
-  throw "err";
-}
-int minimumDeleteSum(string s1, string s2) {
-  int asum = 0;
-  s = s1;t = s2;
-  dp.resize(s.size() + 1,vector<int>(t.size()+1,-1));
-  return mds(0, 0);
-}
+     int lcs(string &s1, string &s2){
+        // ascii sum of retained chars
+        int n1 = s1.size(), n2 = s2.size();
+        vector<vector<int>>dp(n1+1,vector<int>(n2 + 1));
+        for(int i = 0;i < n1;i++){
+            for(int j = 0;j < n2;j++){
+                if(s1[i] == s2[j]){
+                    dp[i + 1][j + 1] = dp[i][j] + s1[i];
+                }else{
+                    dp[i+1][j + 1] = max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        return dp.back().back();
+    }
+    int minimumDeleteSum(string s1, string s2) {
+     // lcs 
+        int total = 0;
+        for(auto &c:s1) total += c;
+        for(auto &c:s2) total += c;
+        return total - 2 * lcs(s1, s2);
+    }
 };
