6f64e642c61d86224711b86749572cab70c87bbc
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2026-06-30 21:52:35 +0530

    Time: 15 ms (32.23%) | Memory: 11 MB (57.43%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        // last occurance
-       vector<int>loc(3,-1); 
-       int ans = 0;
+       // i -> last a
+       // j -> last b
+       // k -> last c
+       //x = min(i,j,k) -> current is the smallest good substr ending here 
+       // ans += x + 1
+
+       // i, j, k
+        vector<int>v(3,-1);
+        int ans = 0;
        for(int i = 0;i< s.size();i++){
-        loc[s[i] - 'a'] = i;
-        int gg = s.size();
-        for(auto &x:loc){
-            gg = min(gg,x);
-        }
-        if(gg == -1)continue;
-        ans += 1 + gg;
+         v[s[i] - 'a'] = i;
+         int x = min({v[0], v[1], v[2]});
+         if(x >= 0){
+            ans += x + 1;
+         }
        }
        return ans;
     }
