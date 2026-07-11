0aa3f79cfc0fdf50542cb7a2d38f2df34cd9cae1
Adarsh Singh <63918341+adarsh-sgh@users.noreply.github.com>
2026-07-12 03:18:40 +0530

    Time: 69 ms (27.67%) | Memory: 134.6 MB (48.17%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,43 +1,49 @@
 class Solution {
 public:
+    vector<vector<int>>adj;
+    vector<int>vis;
+    map<int,int>nodecnt;
+    // return size of group
+    int dfs(int x){
+        assert(vis[x] == 0);
+        vis[x] = true;
+        int cnt = 1;
+        for(auto &a:adj[x]){
+            if(vis[a]) continue;
+            int d = dfs(a);
+            if(d == -1 || nodecnt[x] != nodecnt[a]) cnt = -1;
+            if(cnt != -1)cnt += d;
+        }
+        return cnt;
+    }
 
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-       vector<int>p(n);
-       for(int i = 0;i< n;i++)p[i] = i;
-       function<int(int)> find = [&](int x){
-        if(p[x] == x) return x;
-        return p[x] = find(p[x]);
-       };
-
-       auto Union = [&](int x, int y){
-        p[find(x)] = find(y);
-       }; 
-       // count how many edges a node is connected to
-       vector<int>cnt(n);
-       map<int,set<int>>mp;
-
-       for(auto &e:edges){
-        Union(e[0], e[1]);
-        cnt[e[0]]++;
-        cnt[e[1]]++;
-       }
-
-       for(int i = 0;i<n;i++){
-        mp[find(i)].insert(i);
-       }
-
-       int ans = 0;
-       
-       for(auto &[k,v]:mp){
-        bool gg = true;
-        for(auto &x:v){
-            if(cnt[x] != v.size() - 1){
-                gg = false;
+        // count size using dfs
+        // each node appears size - 1 => ans++;
+        adj.resize(n);
+        vis.resize(n,0);
+        
+        for(auto &e:edges){
+           int x = e[0], y = e[1];
+           nodecnt[x]++;
+           nodecnt[y]++;
+           adj[x].push_back(y);
+           adj[y].push_back(x);
+        }
+        int cnt = 0;
+        // size of current group being dfsed
+        int szc = -1;
+        
+        int ans = 0; 
+        for(int i = 0;i < n;i++){
+            if(vis[i])continue;
+            // find size of group
+            int sz = dfs(i);
+            cout<<i<<"<-i  sz->"<<sz<<'\n';
+            if(sz - 1 == nodecnt[i]){
+                ans++;
             }
         }
-        ans += gg;
-       }
-
-       return ans;
+        return ans;
     }
 };
